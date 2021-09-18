479083177880730d1e7c7214e41dd083f4e6edf3
Rômulo Silva <romulo_lss@hotmail.com>
2021-09-18 12:56:05 -0300

    feat: Function to pass a float and atom. Capturing a number if it is a string

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,9 +1,30 @@
 defmodule ListFilter do
-  def call(list) do
-    list
-    |> Enum.filter(fn elem -> is_number(elem) end)
-    |> Enum.count(fn num -> is_odd(num) end)
-  end
+  def call(list), do: filter(list)
 
-  defp is_odd(num), do: rem(num, 2) != 0
+  defp filter([]), do: 0
+
+  defp filter(list) do
+    list
+    |> Enum.filter(&(!is_atom(&1)))
+    |> Enum.map(fn n ->
+      case n do
+        n when is_integer(n) -> Integer.to_string(n)
+        n when is_float(n) -> Float.to_string(n)
+        _ -> n
+      end
+    end)
+    |> Enum.flat_map(fn elem ->
+      case Integer.parse(elem) do
+        {int, _rest} -> [int]
+        :error -> []
+      end
+    end)
+    |> Enum.map(fn elem ->
+      if rem(elem, 2) != 0 do
+        elem
+      end
+    end)
+    |> Enum.filter(&(!is_nil(&1)))
+    |> length()
+  end
 end
